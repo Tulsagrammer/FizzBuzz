e9bc576642e7f32b66d693a146d873722891ffb0
Eric Chevalier <etech@tulsagrammer.com>
2016-03-19 14:22:33 -0600

    Added "WriterDummy" project to illustrate MEF 2 and lack of compatibility with MEF 1.

---
+++ WriterDummy/DummyWriter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Composition;
+
+namespace FizzBuzz
+{
+    [Export(typeof(IFizzBuzzWriter))]
+    [ExportMetadata("Description", "Dummy Writer")]
+    public class DummyWriter : IFizzBuzzDelegate, IFizzBuzzWriter
+    {
+        public void ResultsFinish()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ResultsItem(TimeSpan timeSpan, string testFunction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Run(int upperLimit, int maxLoops, Action<int, int, IFizzBuzzDelegate> run)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ResultsStart()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TestFinish()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TestItem(int value, string tag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TestStart(string testFunction)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
